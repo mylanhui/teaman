2fc096d9ed8a9edec6bf96bed80fb64ee6f693c2
mylanhui <472303924@qq.com>
2017-12-22 22:33:22 +0800

    hah

--- teaManSys/models.py
+++ teaManSys/models.py
@@ -2,40 +2,50 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class BlogPost(models.Model):
-    title = models.CharField(max_length=150)
-    body = models.TextField()
-    timestamp = models.DateTimeField()
+class college(models.Model):
+    collegeId = models.AutoField(primary_key=True)
+    collegeName = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.collegeName
+class major(models.Model):
+    majorId = models.AutoField(primary_key=True)
+    majorName =models.CharField(max_length=50)
+    collegeId =models.ForeignKey(college,db_column="collegeId")
+
+    def __unicode__(self):
+        return self.majorName
 
 class classList(models.Model):
     classId = models.AutoField(primary_key=True)
     className = models.CharField(max_length=12)
-    college = models.CharField (max_length=20)
-    major = models.CharField(max_length=20)
     studentNum = models.IntegerField()
-
-class student(models.Model):
-    studentId = models.AutoField(primary_key=True)
-    studentName = models.CharField(max_length=10)
-    birthday = models.DateTimeField()
-    studentCourses = models.IntegerField()
-    classId = models.ForeignKey(classList,db_column="classId")
-    studentSex = models.BooleanField(default=0)
+    collegeId = models.ForeignKey(college, db_column="collegeId")
+    majorId = models.ForeignKey(major, db_column="majorId")
 
 class teacher(models.Model):
-    teacherId = models.AutoField(primary_key=True)
+    teacherId = models.CharField(primary_key=True,max_length=8)
     teacherName = models.CharField(max_length=10)
     teacherSex = models.BooleanField(default=0)
+    collegeId = models.ForeignKey(college, db_column="collegeId")
     teacherTitle = models.CharField(max_length=20)
 
-
 class courses(models.Model):
-    coursesId =models.AutoField(primary_key=True)
+    coursesId =models.CharField(primary_key=True,max_length=8)
     coursesName =models.CharField(max_length=40)
     coursesTime =models.CharField(max_length=20)
     credit =models.IntegerField()
     teacherId =models.ForeignKey(teacher,db_column="teacherId")
-    studentId =models.ManyToManyField(student,db_column="studentId")
+
+class student(models.Model):
+    studentId = models.CharField(primary_key=True,max_length=12)
+    studentName = models.CharField(max_length=10)
+    studentCourses = models.IntegerField()
+    classId = models.ForeignKey(classList,db_column="classId")
+    collegeId = models.ForeignKey(college, db_column="collegeId")
+    majorId =models.ForeignKey(major,db_column="majorId")
+    studentSex = models.BooleanField(default=0)
+    coursesId = models.ManyToManyField(courses,db_column="coursesId")
 
 
 class grade(models.Model):
@@ -45,8 +55,9 @@ class grade(models.Model):
     score =models.CharField(max_length=10)
 
 
-class user(models.Model):
+class User(models.Model):
     userId =models.AutoField(primary_key=True)
+    uerName =models.CharField(max_length=20)
     password =models.CharField(max_length=16)
 
 
--- teaManSys/views.py
+++ teaManSys/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render, render_to_response
 
 
 # Create your views here.
+from teaManSys import models
+
+
 def model(req):
 
     return render_to_response('model.html')
@@ -11,7 +14,24 @@ def signIn(req):
     return render_to_response('login/sign-in.html')
 
 def signup(req):
-    return render_to_response('login/sign-up.html')
+    if req.method == 'POST':
+        userName = req.POST["userName"]
+        user = models.User.objects.filter(userName=userName)
+        if user.count() > 0:
+            return HttpResponse(2)
+        passWord = req.POST["passWord"]
+        yanzhengma = req.POST['Input_verify']
+        if req.COOKIES.get('validate', '').upper() != yanzhengma.upper():
+            return HttpResponse(3)
+        userId = uuid.uuid1()
+        try:
+            userSave = models.User(userId=userId, userName=userName, userPwd=passWord)
+            userSave.save()
+            return HttpResponse(1)
+        except Exception as err:
+            return HttpResponse(0)
+    else:
+        return render_to_response('login/sign-up.html')
 
 
 def grade(req):
